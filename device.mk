91f6c1592c5f928ee25f5afe26f13c64fc91ee06
haran <harangovindan@gmail.com>
2017-12-17 15:38:14 +0200

    Santoni: Build OSS Camera hal

--- device.mk
+++ device.mk
@@ -128,14 +128,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
+	
+PRODUCT_PACKAGES += \    
+    camera.msm8937 \
+    libmm-qcamera
 
 # Configstore
 PRODUCT_PACKAGES += \
